9ac087b1f1dcba8f49ebb93bdc7184c98e946a6d
Juan Carlos Bajan <baj20109@uvg.edu.gt>
2021-05-14 17:39:41 -0600

    Comentarios subRutinas1.s

--- subRutinas1.s
+++ subRutinas1.s
@@ -1,3 +1,11 @@
+/*
+ * Autores: Daniel Gonzalez 20293, Juan Carlos Bajan 20109
+ * Modificacion: 14/05/2021
+ * Descripcion: Subrutinas que permiten la conversion de un numero ascii a decimal y 
+ *              concatenacion de dos strings.
+ */
+
+
 .data
 .align 2
 strLen4print: .word 0
@@ -69,34 +77,71 @@ _char2Num:
   BX LR
 
 
+/*
+
+-------------------------------------         SUMA          -----------------------------------------
+
+La Siguiente Subrutina realiza el proceso de adicion recibiendo en los registros r1 y r2 los
+dos valores que se sumaran y en r2 la direccion del registro .word a donde sera almacenado el
+resultado.
+
+Se realizo de forma generica, no realiza ninguna traduccion o verificacion de los valores recibidos
+para que se pueda utilizar en cualquier contexto.
+
+-----------------------------------------------------------------------------------------------------
+
+ */
+
 .global _suma
 _suma:
-  valor1s .req r0
-  valor2s .req r1
-  respuestas .req r2
-  add valor1s,valor2s
-  str valor1s,[respuestas]
+  valor1s .req r0   //R0 recibe "actual"
+  valor2s .req r1   //R1 recibe el valor ingresado por el usuario
+  respuestas .req r2  //r2 recibe el registro en donde se almacenara la respuesta 
+  add valor1s,valor2s   //se suma r1 y r0
+  str valor1s,[respuestas]  // se almacena la respuesta en r2
   .unreq valor1s
   .unreq valor2s
   .unreq respuestas
   BX LR
 
+
+
+/*
+
+---------------------------------         Multiplicacion          -----------------------------------
+
+La Siguiente Subrutina realiza el proceso de multiplicacion recibiendo en los registros r1 y r2 los
+dos valores que se multiplicaran y en r2 la direccion del registro .word a donde sera almacenado el
+resultado.
+
+Se realizo de forma generica, no realiza ninguna traduccion o verificacion de los valores recibidos
+para que se pueda utilizar en cualquier contexto. Por ejemplo, este se utiliza en la subrutina de
+potencia.
+
+-----------------------------------------------------------------------------------------------------
+
+ */
 .global _multiplicacion
 _multiplicacion:
-  mrs r11,CPSR
-  valor1 .req r0
-  valor2 .req r1
-  respuesta .req r2
-  pivote .req r5
-  contador .req R4
-  mov pivote,valor1
-  mov contador,#1
+  mrs r11,CPSR  // Para que los cambios en el CPSR en esta subrutina no afecten al main,
+                //  se almacena el valor proveniente del main a r11 para posteriormente
+                //  retornarlo a su valor inicial y no afectar los procesos del archivo principal.
+  valor1 .req r0  // R0 recibe el valor que se multiplicara
+  valor2 .req r1  // R1 reicbe el valor que multiplicara a R0
+  respuesta .req r2 // R2 recibe el valor en donde se almacenara la respuesta
+  pivote .req r5  // Como, la multiplicacion es un valor n cantidad de veces sumado con 
+                  //si mismo, inicialmente se almacena una copia de r0 en r5 para que
+                  //ese primer valor no se pierda.
+  contador .req R4  //Este registro indica si se realiza o no el loop de nuevo. Cuando este
+                    //contador sea igual a r2, el loop finaliza.
+  mov pivote,valor1 //se guarda una copia de r0 en r5
+  mov contador,#1 //El contador debe iniciar en 1
   loop:
-    add valor1,pivote
-    add contador,#1
-    cmp contador,valor2
+    add valor1,pivote //Se le suma a r0, su valor inicial (pivote)
+    add contador,#1 // Se incrementa el contador
+    cmp contador,valor2 //Si el contador es igual a r1 entonces finaliza el loop
     blt loop
-  str valor1,[respuesta]
+  str valor1,[respuesta]  //Se almacena la respuesta en r0
   .unreq valor1
   .unreq valor2
   .unreq respuesta
@@ -105,18 +150,33 @@ _multiplicacion:
   MSR CPSR,r11
   BX LR
 
+/*
 
+-------------------------------------         Modulo          ---------------------------------------
+
+La Siguiente Subrutina realiza el proceso de modulo recibiendo en los registros r1 y r2 los
+dos valores que se operaran y en r2 la direccion del registro .word a donde sera almacenado el
+resultado.
+
+Se realizo de forma generica, no realiza ninguna traduccion o verificacion de los valores recibidos
+para que se pueda utilizar en cualquier contexto.
+
+-----------------------------------------------------------------------------------------------------
+
+ */
 .global _modulo
 _modulo:
-  mrs r11,CPSR
-  valor1m .req r0
-  valor2m .req r1
-  respuestam .req r2
+  mrs r11,CPSR// Para que los cambios en el CPSR en esta subrutina no afecten al main,
+              //  se almacena el valor proveniente del main a r11 para posteriormente
+              //  retornarlo a su valor inicial y no afectar los procesos del archivo principal.
+  valor1m .req r0 //R0 recibe el dividendo
+  valor2m .req r1 //R1 recibe el divisor
+  respuestam .req r2  //R2 recibe el valor en donde se almacenara la respuesta
   loop1:
-    sub valor1m,valor1m,valor2m
-    cmp valor1m,valor2m
-    bge loop1
-  str valor1m,[respuestam]
+    sub valor1m,valor1m,valor2m //Se le resta al dividendo el divisor
+    cmp valor1m,valor2m //Se verifica si el dividendo es mayor o igual que el divisor
+    bge loop1 //En caso que si sea mayor o igual, se realiza de nuevo el loop
+  str valor1m,[respuestam]  // Se almacena r0 en r2
   .unreq valor1m
   .unreq valor2m
   .unreq respuestam
@@ -124,22 +184,40 @@ _modulo:
   BX LR
 
 
+/*
+
+-------------------------------------         Modulo          ---------------------------------------
+
+La Siguiente Subrutina realiza el proceso de potencia recibiendo en los registros r1 y r2 los
+dos valores que se operaran y en r2 la direccion del registro .word a donde sera almacenado el
+resultado.
+
+Se realizo de forma generica, no realiza ninguna traduccion o verificacion de los valores recibidos
+para que se pueda utilizar en cualquier contexto. Utiliza la subrutina multiplicacion para operar
+los valores.
+
+-----------------------------------------------------------------------------------------------------
+
+ */
 .global _potencia
 _potencia:
-  PUSH {LR}
-  valor1a .req r0
-  exponente .req r9
-  respuestaa .req R2
-  contadora .req R8
-  mov exponente,r1
-  mov contadora,#1
-  mov r1,valor1a
+  PUSH {LR} // Como dentro de esta subrutina se llama otra subrutina, se debe hacer un PUSH y POP del LR en el stack.
+  valor1a .req r0 // R0 recibe el valor que se elevara a la potencia
+  exponente .req r9 // R9 recibe la potencia
+  respuestaa .req R2  //R2 recibe el valor donde se almacenara la respuesta
+  contadora .req R8 // R8 es un contador para verificar si se ha realizado el proces
+                    // la cantidad de veces necesarias
+  mov exponente,r1  // Se realiza una copia de r1 en r9 para no estropear el proceso de la subrutina multiplicacion
+  mov contadora,#1  // El contador inicia en 1
+  mov r1,valor1a  // Como la subrutina multiplicacion recibe uno de sus factores en r1 y al elevar la potencia se esta 
+                  // multiplicando un factor por si mismo, se copia r0 en r1 para completar este proceso
   loop2:
-    bl _multiplicacion
-    add contadora,#1
-    cmp contadora,exponente
-    blt loop2
-  str valor1a,[respuestaa]
+    bl _multiplicacion  // Se llama a la subrutina multiplicacion
+    add contadora,#1  // Se incrementa el contador
+    cmp contadora,exponente // Se compara el contador con el exponente ya que el exponente dice la cantidad de veces que
+                            // Se repetira el proceso
+    blt loop2 // En caso sea menor, se vuelve a realizar en proceso
+  str valor1a,[respuestaa]  // Se almacena la respuesta en r2
   .unreq respuestaa
   .unreq contadora
   .unreq valor1a
